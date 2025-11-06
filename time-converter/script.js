2c7feed0dddd79c41755297d6c8917f1b5c4fdf7
Phntme <phntmecode@gmail.com>
2025-11-06 20:16:48 +0700

    Update Validasi + Message
    update kali ini nambahin validasi biar user gabisa masukin input kosong atau huruf. jika user maksa masukin, bakal muncul error message

--- time-converter/script.js
+++ time-converter/script.js
@@ -1,12 +1,34 @@
 const btn = document.getElementById("btn"),
   input = document.getElementById("time"),
-  output = document.getElementById("output");
+  output = document.getElementById("output"),
+  errorMsg = document.getElementById("error");
 
 function convert(value) {
-  const [hour, minute, second] = value.split(":").map(Number);
-  return hour * 3600 + minute * 60 + second;
+  waktu = value.split(":").map(Number);
+  if (waktu.length === 1) {
+    return waktu;
+  } else if (waktu.length === 2) {
+    const [menit, detik] = waktu;
+    return menit * 60 + detik;
+  } else {
+    const [jam, menit, detik] = waktu;
+    return jam * 3600 + menit * 60 + detik;
+  }
+}
+
+function validasi(value) {
+  waktu = value.split(":").map(Number);
+  if (!value.trim() || waktu.some(isNaN)) {
+    errorMsg.classList.add("active");
+    setTimeout(() => {
+      errorMsg.classList.remove("active");
+    }, 1500);
+    return false;
+  }
+  return true;
 }
 
 btn.addEventListener("click", () => {
-  output.value = convert(input.value);
+  if (!validasi(input.value)) return;
+  output.value = convert(input.value) + " Detik";
 });
