301093002193dc6d31f0d289b48ef5017ba5fd61
Phntme <phntmecode@gmail.com>
2025-11-08 17:40:47 +0700

    nambahin fitur upgrade dengan harga 50

--- waifu-clicker/script.js
+++ waifu-clicker/script.js
@@ -1,14 +1,24 @@
 const score = document.getElementById("score"),
-  scoreBtn = document.getElementById("score-btn");
+  scoreBtn = document.getElementById("score-btn"),
+  upgradeFinger = document.getElementById("upgrade-finger"),
+  minusPop = document.querySelector(".currency-minus"),
+  minusValue = document.getElementById("minus-value"),
+  tapStatus = document.getElementById("tap-status");
+
+// harga upgrade
+const fingerCost = 50;
 
 let scorePoint = 0;
 let canHold = true,
-  canPress = true;
+  canPress = true,
+  clickPoint = 1;
 
+// fungsi nambah score
 function addScore() {
-  score.textContent = scorePoint += 1;
+  score.textContent = scorePoint += clickPoint;
 }
 
+// fungsi animasi
 function btnAnimation() {
   scoreBtn.classList.add("btn-active");
   setInterval(() => {
@@ -16,6 +26,29 @@ function btnAnimation() {
   }, 100);
 }
 
+// fungsi duit kurang bjirla
+function duitKurang(kurangBerapa) {
+  let kurang = scorePoint - kurangBerapa;
+  minusPop.classList.add("show");
+  setTimeout(() => {
+    minusPop.classList.remove("show");
+  }, 1500);
+  return Math.abs(kurang);
+}
+
+// fungsi upgrade finger training
+function fingerUpgrade() {
+  clickPoint += 1;
+}
+
+// fungsi berhasil upgrade
+function berhasilUpgrade(harga) {
+  scorePoint -= harga;
+  score.textContent = scorePoint;
+  fingerUpgrade();
+}
+
+// kalo button skor diklik
 scoreBtn.addEventListener("click", () => {
   btnAnimation();
   addScore();
@@ -49,4 +82,19 @@ document.addEventListener("keyup", function (event) {
   if (event.code === "Space") {
     canPress = true;
   }
+});
+
+// upgrade click
+function fingerTraining() {
+  if (scorePoint >= fingerCost) {
+    berhasilUpgrade(fingerCost);
+    tapStatus.textContent = clickPoint;
+  } else {
+    minusValue.textContent = duitKurang(fingerCost);
+  }
+}
+
+// kalo upgrade finger diklik
+upgradeFinger.addEventListener("click", () => {
+  fingerTraining();
 });
